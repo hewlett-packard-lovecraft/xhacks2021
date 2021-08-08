cd2e196654638a7672675801ad41b469f3126dd1
Haoming Xia <mrmist3r311@gmail.com>
2021-08-08 00:07:02 -0700

    added music script

--- game/page2/index.js
+++ game/page2/index.js
@@ -1,28 +1,40 @@
-/*
- * assuming the cost of plywood is $10/foot
- * Cost per crate = ( 10 * 2lw + 2hw + 2lh)
- * Revenue = lwh * 15
- */
-
-
-//document.getElementById("v4_6").addEventListener("submit", handle_submit);
-
 function handle_submit() {
   const length = document.getElementById("length").value;
   const width = document.getElementById("width").value;
 
+  switch(document.getElementById("supplier").value) {
+    case "richmond":
+      document.getElementById("sscore").innerHTML = 5;
+      break;
+    case "windsor":
+      document.getElementById("sscore").innerHTML = 7;
+      break;
+    case "smith":
+      document.getElementById("sscore").innerHTML = 10;
+      break;
+  }
+
   console.log(width);
   console.log(length);
   const height = 20; // hard coded to 20ft
-  const time_per_crate = 1; // hours
+  const time_per_crate = length * width * 0.2; 
 
-  const cost_per_crate = 10 * ( 2*length*width + 2*length*height + 2*width*height);
-  const revenue_per_crate = (length * width * height) * 36; // profit per crate 
-  const profit_per_crate_time = (revenue_per_crate - cost_per_crate) / (time_per_crate);
+  const cost_per_crate =
+    10 * (2 * length * width + 2 * length * height + 2 * width * height);
+  const revenue_per_crate = length * width * height * 36; // profit per crate
+  const profit_per_crate_time = (revenue_per_crate - cost_per_crate) / time_per_crate;
+  const profit_per_crate = revenue_per_crate - cost_per_crate;
 
   document.getElementById("profit_per_crate_time").innerHTML = profit_per_crate_time;
   document.getElementById("revenue_per_crate").innerHTML = revenue_per_crate;
   document.getElementById("profit_per_crate").innerHTML = profit_per_crate;
+  return false; // needs to return false or else it reloads the page
+}
 
-  console.log("profit_per_crate_time " + profit_per_crate_time);
+function validate(e) {
+  if (e.value <= 0) {
+    alert("Values must be greater than zero")
+  }
+  e.preventDefault();
+  return false;
 }
